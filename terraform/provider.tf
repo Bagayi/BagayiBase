5334ceda683418ea17c416a5f1ecd31fcef68c1f
fmwangi <fmwangi@microsoft.com>
2023-09-21 09:11:13 +0000

    use mork keys.

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-    access_key                  = "AKIATYUVCWESJE23SS6E"
+    access_key                  = "mock_access_key"
     region                      = "us-east-1"
-    secret_key                  = "fIgqKrNFQYXgc9mnViV4vxoH5topYkMb5jmdlpVn"
+    secret_key                  = "mock_secret_key"
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_requesting_account_id  = true
